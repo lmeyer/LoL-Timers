0681d37cec67cc700de22d4433ae4384e719b7f7
Ludovic Meyer <ludovic.meyer@pixel-cookers.com>
2012-04-08 16:23:11 +0200

    Project first commit

---
+++ js/script.js
@@ -0,0 +1,44 @@
+/* Author: Ludovic Meyer
+
+ */
+
+$(document).ready(function() {
+
+	var timers = {
+		wraiths : 50,
+		wolves  : 60,
+		golems  : 60,
+
+		blue    : 300,
+		red     : 300,
+
+		dragon  : 360,
+		nashor  : 420
+	};
+	
+	$.countdown.setDefaults({
+		compact: true, 
+		format: 'MS',
+		description: '',
+		onTick: highlightLast10,
+		onExpiry: cdDestroy
+	});
+
+
+	$(".cd").click(function() {
+		$(this).countdown('destroy').countdown({
+			until: +timers[ $(this).attr('class').substr(3) ]
+		});
+	});
+	
+	function highlightLast10(periods) { 
+	    if ($.countdown.periodsToSeconds(periods) == 5) { 
+	        $(this).effect('pulsate', 1000);
+	    } 
+	}
+	
+	function cdDestroy() { 
+		$(this).countdown('destroy'); 
+	}
+	
+});
