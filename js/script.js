bab02d09948fcca6f501cd9c84f64df7d10e8d5f
Ludovic Meyer <ludovic.meyer@pixel-cookers.com>
2012-04-11 02:07:48 +0200

    Increase readability, add wards creation and countdown

--- js/script.js
+++ js/script.js
@@ -5,18 +5,20 @@
 $(document).ready(function() {
 
 	var timers = {
-		wraiths : 50,
-		wolves  : 60,
-		golems  : 60,
+		wraiths   : 50,
+		wolves    : 60,
+		golems    : 60,
 
-		blue    : 300,
-		red     : 300,
+		blue      : 300,
+		red       : 300,
 
-		dragon  : 360,
-		nashor  : 420,
+		dragon    : 360,
+		nashor    : 420,
 		
 		ordertower: 300,
-		chaostower: 300
+		chaostower: 300,
+		
+		wards     : 180
 	};
 	
 	$.countdown.setDefaults({
@@ -29,21 +31,41 @@ $(document).ready(function() {
 
 
 	$(".cd").click(function() {
-		$(this).countdown('destroy').countdown({
-			until: +timers[ $(this).attr('class').substr(3) ]
-		});
+		cdCreate($(this));
 	});
+	
+	$(".map").click(function( e ) {
+		if(!$(e.target).hasClass('map')) return;
+		
+		var left = e.pageX-12;
+		var top = e.pageY-12;
+		
+		var $new_ward = $('<div class="cd wards" style="left:'+left+'px;top:'+top+'px"></div>');
+		$("body").append($new_ward)
+		cdCreate($new_ward);
+	});
+	
+	
 	
 	function highlightLast10(periods) { 
 	    if ($.countdown.periodsToSeconds(periods) == 5) { 
 	        $(this).effect('pulsate', 1000);
-	        $(this).addClass('highlight');
+        	$(this).addClass('highlight');
 	    } 
+	}
+	
+	function cdCreate(elem) {
+		elem.countdown('destroy').countdown({
+			until: +timers[ elem.attr('class').substr(3) ]
+		});
 	}
 	
 	function cdDestroy() { 
 		$(this).countdown('destroy');
 		$(this).removeClass('highlight');
+		if($(this).hasClass('wards')){
+			$(this).fadeOutAndRemove('slow');
+		};
 	}
 
 	window.setTimeout(function() {
@@ -55,4 +77,9 @@ $(document).ready(function() {
 		bubble.showIfAllowed();
 	}, 1000);
 	
+	jQuery.fn.fadeOutAndRemove = function(speed){
+	    $(this).fadeOut(speed,function(){
+	        $(this).remove();
+	    })
+	}
 });
