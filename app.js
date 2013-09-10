3da43aeb561cfb2b1dc522d714e02681e8c5638e
Ludovic Meyer <ludovic.meyer@pixel-cookers.com>
2013-09-10 03:02:42 +0200

    modify log level

--- app.js
+++ app.js
@@ -4,6 +4,7 @@
 var io = require('socket.io').listen(8080)
 var connected = {};
 var default_room = 'default';
+io.set('log level', 1); // reduce logging
 io.sockets.on('connection', function (socket) {
 
 	/**
@@ -30,8 +31,6 @@ io.sockets.on('connection', function (socket) {
 			room = default_room;
 		}
 		socket.room = room;
-		console.log(socket.room);
-
 
 		if(!connected[socket.room]) {
 			connected[socket.room] = 0;
@@ -40,6 +39,7 @@ io.sockets.on('connection', function (socket) {
 		socket.join(socket.room);
 		connected[socket.room] = connected[socket.room]+1
 		io.sockets.in(socket.room).emit('connected', connected[socket.room])
+		console.log(socket.room + ' : ' + connected[socket.room]);
 	});
 
 	/**
